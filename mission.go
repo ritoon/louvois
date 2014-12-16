0b000c5ea80778136d0e0241d1f36848c8d507ac
Henri Lepic <henrilepic@gmail.com>
2014-12-16 11:50:08 +0100

    ajout d'entitées

--- mission.go
+++ mission.go
@@ -2,16 +2,45 @@ package louvois
 
 import "time"
 
+type MeansType struct {
+	title string
+}
+
+type MeansCategory struct {
+	title string
+}
+
+// moyens matériel ou humain
+type Means struct {
+	title         string
+	meansType     MeansType
+	meansCategory MeansCategory
+}
+
 type Bonus struct {
 	title        string
 	amount       int
 	isPercentage bool
 }
 
+type TodoType struct {
+	title string
+}
+
+// Elements à réaliser
+type Todo struct {
+	title     string
+	todotype  TodoType
+	bonus     []Bonus
+	state     string
+	dateStart time.Date
+	dateStop  time.Date
+}
+
 type Mission struct {
 	title     string
 	risk      int
-	bonus     Bonus
+	todos     []Todo
 	dateStart time.Date
 	dateStop  time.Date
 }
--- soldier.go
+++ soldier.go
@@ -8,6 +8,27 @@ type Rank struct {
 	dateStop  time.Date
 }
 
+// amrée air, terre, mere
+type Army struct {
+	title string
+}
+
+type Job struct {
+	army            Army
+	soldierCategory SoldierCategory
+	soldierType     SoldierType
+	ranks           Rank
+	dateStart       time.Date
+	dateStop        time.Date
+}
+
+type SoldierCategory struct {
+	title string
+}
+type SoldierType struct {
+	title string
+}
+
 type SoldierGroup struct {
 	title    string
 	soldiers []Soldier
@@ -16,12 +37,45 @@ type SoldierGroup struct {
 type Soldier struct {
 	firstName     string
 	lastName      string
-	ranks         []Rank
+	job           []Job
+	adress        []Adress
+	phone         []Phone
 	missions      []Mission
 	vacations     []Vacation
 	diseases      []Disease
 	responsibleOf []Soldier
 	underOrderOf  []Soldier
+	hasFamily     bool
+	nChildren     int
+	dateStartArmy time.Date
 	dateBorn      time.Date
 	dateDeath     time.Date
+}
+
+type Country struct {
+	title string
+}
+
+type AdressType struct {
+	title string
+}
+
+type Adress struct {
+	adressType   AdressType
+	country      Country
+	streetName   string
+	streetNumber string
+	bat          string
+	special      string
+	dateStart    time.Date
+	dateStop     time.Date
+}
+
+type PhoneType struct {
+	title string
+}
+
+type Phone struct {
+	number    string
+	phoneType PhoneType
 }
