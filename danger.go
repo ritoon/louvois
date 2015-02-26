5cccf139d8b8a76010f55e37e8d43b8943f7a370
Ritoon <henrilepic@gmail.com>
2015-02-26 22:18:14 +0100

    ajout de commentaires sur danger

--- danger.go
+++ danger.go
@@ -11,13 +11,20 @@ type DangerCategory struct {
 }
 
 type Danger struct {
-	level          int
-	dangerType     DangerType
-	dangerCategory DangerCategory
-	dateStart      time.Time
-	dateStop       time.Time
+	level int // niveau de danger
+
+	dangerType DangerType // type de danger
+
+	dangerCategory DangerCategory // catégorie
+
+	dateStart time.Time // date début
+
+	dateStop time.Time // date fin
 }
 
+/*
+  Permet d'ajouter un coef de danger
+*/
 func (d *Danger) dangerCoef() float64 {
 	switch {
 	case d.level == 0:
